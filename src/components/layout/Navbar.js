780faa3a8db000b8defc6d105f289fc1c6e99661
Xantic44 <51379252+Xantic44@users.noreply.github.com>
2019-08-06 20:44:57 +0200

    drag n drop
    search song lyrics finder. shows about only 40% lyrics of a song cuz of the free plan.
    This can only be used for STUDIES and self-learning.

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-dark bg-dark mb-5">
+            <span className="navbar-brand mb-0 h1 mx-auto"><i>iLyrics</i></span> 
+            
+                <div>
+                <p>made by love from Sweden </p>    
+                </div>
+                
+        </nav>
+        
+       
+    );
+};
+export default Navbar;
